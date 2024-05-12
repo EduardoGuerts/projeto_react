d92904ebc6b0f44f7b722fa04bb045198586536b
Eduardo Batista Guerts <eduardoguerts@hotmail.com>
2024-05-12 16:51:46 -0300

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Formulario from "./pages/Formulario";
 import Login from "./pages/Login";
 import AreaProfessor from "./pages/AreaProfessor";
@@ -10,7 +10,7 @@ function App() {
     return(
         <>
             <div>
-                <HashRoute>
+                <HashRouter>
                 <Routes>
                     <Route path="/projeto_react" element={<Formulario />} />
                     <Route path="/Login" element={<Login />} />
@@ -18,7 +18,7 @@ function App() {
                     <Route path="/Gabarito" element={<Gabarito />} />
                     <Route path="/DetalhesAluno/:id" element={<DetalhesAluno />} />
                 </Routes>
-            </HashRoute>
+            </HashRouter>
             </div>
         </>
     )
